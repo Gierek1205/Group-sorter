e314de79810e625f10a6f852c2de8fe6e64c65f1
Gierek1205 <gierek1205@gmail.com>
2016-12-11 14:03:19 +0100

    Program Release

---
+++ MATMA/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace MATMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(202, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Przelicz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ilosc uczniow w klasie";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ilość grup";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CausesValidation = false;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(337, 37);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(63, 304);
+            this.checkedListBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.WordWrap = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(41, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.CausesValidation = false;
+            this.checkedListBox2.CheckOnClick = true;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(406, 37);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(63, 304);
+            this.checkedListBox2.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(365, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Nieobecni";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(15, 93);
+            this.listView1.Margin = new System.Windows.Forms.Padding(10);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(302, 248);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 351);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Losowanie grup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ MATMA/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MATMA
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        Random rnd = new Random();
+
+        public int trycatch(string tocheck)
+        {
+            int result = 0;
+            
+            
+            try
+            {
+                result = Convert.ToInt32(tocheck);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Zły format (podaj liczbe)");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Przekroczony zakres (podaj liczbe)");
+            }
+            
+
+            return result;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkedListBox1.Hide();
+            checkedListBox2.Hide();
+            
+            checkBox1.Hide();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            int ilosc_uczniow = Convert.ToInt32(textBox1.Text);
+
+            if (checkBox1.Checked )
+            {
+
+
+                for (int x = 1;( x <= ilosc_uczniow && x <= 20); ++x)
+                {
+                    checkedListBox1.Items.Add(Convert.ToString(x));
+                }
+                if (ilosc_uczniow > 20)
+                {
+                    for (int x = 21; x <= ilosc_uczniow; x++)
+                    {
+                        checkedListBox2.Items.Add(Convert.ToString(x));
+                    }
+                }
+
+
+                checkedListBox1.Show();
+                if(ilosc_uczniow > 20)
+                {
+                    checkedListBox2.Show();
+                }
+
+            }
+            else
+            {
+                checkedListBox1.Items.Clear();
+                checkedListBox2.Items.Clear();
+                checkedListBox1.Hide();
+                checkedListBox2.Hide();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+            int val = 0;
+            checkedListBox1.Items.Clear();
+
+            if (textBox1.TextLength >= 1)
+            {
+                val = trycatch(textBox1.Text);
+
+            }
+            
+            if (val >= 1)
+            {
+
+                checkBox1.Show();
+
+
+            }
+            else
+            {
+                checkBox1.Hide();
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            int grup_val = 0;
+            if (textBox2.TextLength >= 1)
+            {
+                    grup_val = trycatch(textBox2.Text);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listView1.Items.Clear();
+            List<int> obecni_list = new List<int>();
+            List<int> nieobecni_list = new List<int>();
+
+            foreach(string nieobecny in checkedListBox1.CheckedItems)
+            {
+                nieobecni_list.Add(Convert.ToInt32(nieobecny));
+            }
+
+            foreach (string nieobecny in checkedListBox2.CheckedItems)
+            {
+                nieobecni_list.Add(Convert.ToInt32(nieobecny));
+            }
+
+
+            int ile_grup = trycatch(textBox2.Text);
+            
+            int liczba_uczniow = trycatch(textBox1.Text);
+
+
+            //Lista obecnych
+            for(int x = 1; x <= liczba_uczniow; x++)
+            {
+                if (!nieobecni_list.Contains(x))
+                {
+                    obecni_list.Add(x);
+                }
+
+            }
+
+
+            int obecni = obecni_list.Count;
+            int ile_osob_w_grupie = obecni / ile_grup;
+            int reszta_w_grupach = obecni % ile_grup;
+            
+            
+
+            
+            for (int x = 1; x <= ile_grup; x++)
+            {
+                string nazwa_grupy = "";
+                int ile_w_jednej_grupie = obecni / ile_grup;
+
+                if (reszta_w_grupach > 0)
+                {
+                    ile_w_jednej_grupie = (obecni / ile_grup) + 1;
+                }
+
+                List<int> grup = new List<int>();
+
+                do
+                {
+                    int wylosowany = rnd.Next(1, liczba_uczniow+1);
+
+                    if (obecni_list.Contains(wylosowany))
+                    {
+                        grup.Add(wylosowany);
+                        obecni_list.Remove(wylosowany);
+                    }
+
+
+                } while (grup.Count < ile_w_jednej_grupie);
+
+                foreach(int numer in grup)
+                {
+                    nazwa_grupy += Convert.ToString(numer + ", ");
+                }
+
+                if (reszta_w_grupach > 0)
+                {
+                    reszta_w_grupach -= 1;
+                }
+                listView1.Items.Add(nazwa_grupy);
+
+            }
+            
+
+
+
+
+
+
+        }
+
+        
+    }
+}
